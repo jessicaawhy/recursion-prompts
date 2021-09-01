f46dfda7850c9a7d5e025345986d367293c97419
Jessica Yang <jessicaawhy@gmail.com>
2021-08-31 18:20:56 -0700

    Implement brute force recursive solution of multiply

--- src/recursion.js
+++ src/recursion.js
@@ -160,6 +160,19 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+  if (y === 0 || x === 0) {
+    return 0;
+  }
+
+  if (x > 0 && y > 0) {
+    return y + multiply(x - 1, y);
+  } else if (x < 0 && y < 0) {
+    return multiply(x + 1, y) - y;
+  } else if (x > 0 && y < 0) {
+    return multiply(x - 1, y) + y;
+  } else {
+    return multiply(x + 1, y) - y;
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
