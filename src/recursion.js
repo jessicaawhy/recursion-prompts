8e7330a6fd1f23271fca3c054c1ac4288625c754
Jessica Yang <jessicaawhy@gmail.com>
2021-09-02 00:31:13 -0700

    Implement buildList

--- src/recursion.js
+++ src/recursion.js
@@ -265,6 +265,9 @@ var reverseArr = function(array) {
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  var result = [value];
+  if (length === 1) return result;
+  return result.concat(buildList(value, length - 1));
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
